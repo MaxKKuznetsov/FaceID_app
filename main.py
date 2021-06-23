83335a70f8ebeeb49063e3a6161af6b84a443e7f
MaxKKuznetsov <maxk.kuznetsov@gmail.com>
2021-06-23 17:34:55 +0300

    test commit2

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt5 import QtWidgets
+
+from Model.model import Model
+from Controller.controller import Controller
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+
+    # создаём модель
+    model = Model()
+
+    # создаём контроллер и передаём ему ссылку на модель
+    controller = Controller(model)
+
+    app.exec()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
